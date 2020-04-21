0b37785994cf7fa8bea653b944abcb5dd4d1cacd
RL444 <remi.laure@outlook.com>
2020-04-21 11:42:47 +0200

    collision and Gameplay update
    finishing modification of sprites ( all at 40x40 now)
    collision with all and ouside maze
    managing of object pickup
    end game

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import pygame
 #dimensions in pixel
 WIDTH = 640
 HEIGHT = 640
+WIDTH_OPTION = 40
+HEIGHT_OPTION  = 40
 FPS = 30
 
 def load_assets(folder_path):
@@ -45,7 +47,7 @@ class Maze:
         #Load map, create a list of Sprite instance corresponding to the status wall or floor with right position
         self.map = self.__init_map(folder_path)
         self.guardian = Sprite(assets["guardian"],(14*40,8*40))
-        self.objects = self.__init_objects()
+        self.items = self.__init_objects()
 
     def __init_map(self, folder_path):
         """Load map and create Sprite instance for background
@@ -72,35 +74,119 @@ class Maze:
         while len(results) < 3:
             x = randint(0, 14)
             y = randint(0, 14)
-            if (self.map[x+y*15].image == assets["floor"] and
+            if (self.compare_sprite((x,y), "floor") and
                 not self.guardian.compare_pos((x,y))):
                 results.append(Sprite(assets[sprite[len(results)]], (x*40, y*40)))
         return results
+
+    def compare_sprite(self, pos, sprite_name):
+        """ Compare sprite in pos with the sprite given
+        return a boolean"""
+        if (self.map[pos[0]+pos[1]*15].image == assets[sprite_name]):
+            return True
+        return False
 
 class Player(Sprite):
     """Special Sprite with additional function to allow movement depending on key press by player"""
     def __init__(self, img, pos):
         super().__init__(img, pos)
+        self.moving = False
+        self.target = [pos[0], pos[1]]
+        self.speed = [0,0]
+        self.alive = True
+        self.escape = False
+        self.items = []
 
 
     def update(self):
+        if self.moving:
+            self.rect.x += self.speed[0]
+            self.rect.y += self.speed[1]
+            if self.target == [self.rect.x, self.rect.y]:
+                self.moving = False
+                self.speed[0] = 0
+                self.speed[1] = 0
+        else:
+            self.check_guardian()
+            if len(self.items) < 3:
+                self.check_objects()
+            else:
+                self.items.append(Sprite(assets["syringe"], (5*40,15*40)))
+                #to be modify
+                all_sprites.add(self.items[3])
+            self.check_mov()
+
+    def check_guardian(self):
+        """ Check if guardian if guardian is in cell and depending on number of objects in possession of player
+        kill guardian or player"""
+        if (maze.guardian.rect.x == self.rect.x and 
+        maze.guardian.rect.y == self.rect.y):
+            if len(self.items) == 3:
+                self.escape = True
+            else:
+                self.alive = False
+
+    def check_objects(self):
+        for i,item in enumerate(maze.items[:]):
+            if (item.rect.x == self.rect.x and
+            item.rect.y == self.rect.y):
+               self.items.append(maze.items.pop(i))
+               #Modify position of items to place it our of maze
+               index = len(self.items) - 1
+               self.items[index].rect.x = index * 40
+               self.items[index].rect.y = 15 * 40
+               #Not possible to have two items on same place so there is no need to continue test
+               break
+
+    def check_mov(self):
+        """fonction that check the validity of move of the player and change target, speed and movement status if movement allow"""
         keystate = pygame.key.get_pressed()
-        if keystate[pygame.K_LEFT]:
-            self.rect.x -= 40
-        if keystate[pygame.K_RIGHT]:
-            self.rect.x += 40
-        if keystate[pygame.K_UP]:
-            self.rect.y -= 40
-        if keystate[pygame.K_DOWN]:
-            self.rect.y += 40
-        if self.rect.right > WIDTH:
-            self.rect.right = WIDTH
-        if self.rect.left < 0:
-            self.rect.left = 0
-        if self.rect.top  < 0:
-            self.rect.top = 0
-        if self.rect.bottom > HEIGHT:
-            self.rect.bottom = HEIGHT
+        if (keystate[pygame.K_LEFT] and
+        self.rect.left > 0 and
+        maze.compare_sprite(self.pos_left(), "floor")):
+            self.target[0] -= 40
+            self.speed[0] = -10
+            self.moving = True
+
+        if (keystate[pygame.K_RIGHT] and
+        self.rect.right < WIDTH - WIDTH_OPTION and 
+        maze.compare_sprite(self.pos_right(), "floor")):
+            self.target[0] += 40
+            self.speed[0] = +10
+            self.moving = True
+
+        if (keystate[pygame.K_UP] and
+        self.rect.top > 0 and
+        maze.compare_sprite(self.pos_up(), "floor")):
+            self.target[1] -= 40
+            self.speed[1] = -10
+            self.moving = True
+
+        if (keystate[pygame.K_DOWN] and
+        self.rect.bottom < HEIGHT - HEIGHT_OPTION and
+        maze.compare_sprite(self.pos_down(), "floor")):
+            self.target[1] += 40
+            self.speed[1] = +10
+            self.moving = True
+
+    def pos_left(self):
+        """Return tuple of index from sprite at the left of player """
+        return ((self.rect.x-40)//40, self.rect.y//40)
+
+
+    def pos_right(self):
+        """Return tuple of index from sprite at the left of player """
+        return ((self.rect.x+40)//40, self.rect.y//40)
+
+
+    def pos_up(self):
+        """Return tuple of index from sprite at the left of player """
+        return (self.rect.x//40, (self.rect.y-40)//40)
+
+
+    def pos_down(self):
+        """Return tuple of index from sprite at the left of player """
+        return (self.rect.x//40, (self.rect.y+40)//40)
 
 
 # initialize pygame and create Game Window
@@ -115,14 +201,14 @@ assets = load_assets(ressources)
 
 #Instance for main classes
 maze = Maze(ressources)
-player = Player(assets["player"], (1*40, 1*40))
+player = Player(assets["player"], (2*40, 1*40))
 
 #Load Sprites in group
 all_sprites = pygame.sprite.Group()
 for sprite in maze.map:
     all_sprites.add(sprite)
 all_sprites.add(maze.guardian)
-for sprite in maze.objects:
+for sprite in maze.items:
     all_sprites.add(sprite)
 all_sprites.add(player)
 
@@ -136,6 +222,14 @@ while running:
         # check for closing window
         if event.type == pygame.QUIT:
             running = False
+
+    if player.escape:
+        running = False
+        print("You Win")
+
+    if not player.alive:
+        running = False
+        print("You Lose")
 
     # Update all sprites status
     all_sprites.update()
