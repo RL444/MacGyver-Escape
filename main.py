dfb79642ee00fe8410da34b7f330564e6df22703
Rémi Laure <remi.laure@outlook.com>
2020-04-27 13:35:55 +0200

    Enclosed everything in class
    rewrite the main file with every information inside class
    add Game class to manage workflow
    no movement
    no objects...

--- main.py
+++ main.py
@@ -1,249 +1,155 @@
 import json
-import sys
 
-from random import randint
-
-from pathlib import Path
 import pygame
+from pathlib import Path
 
-#Main parameter for pygame window
-#dimensions in pixel
-WIDTH = 640
-HEIGHT = 640
-WIDTH_OPTION = 40
-HEIGHT_OPTION  = 40
-FPS = 30
-SPRITE_HEIGHT = 40
-SPRITE_WIDTH = 40
+import init
 
-def load_assets(folder_path):
-    """Load and convert all png file of a folder into a dictionnary to be returned"""
-    storage = {}
-    for path in folder_path.glob('*.png'):
-        storage[path.stem] = pygame.image.load(str(path)).convert()
 
-    return storage
+class Game:
+    """ Class managing the state machine and general information of the Game"""
+    def __init__(self):
+        # Initialize Pygame basics
+        pygame.init()
+        self.screen = pygame.display.set_mode((1000, 1000))
+        pygame.display.set_caption("My Game")
+        self.clock = pygame.time.Clock()
+
+        self.status = init.MENU
+
+        # Load all information
+        self.__load_data()
+
+    def __load_assets(self, folder, files):
+        """Load and convert all required png file of the folder
+        return a dict of the surface created"""
+        storage = {}
+        try:
+            for path in [folder / str(name + ".png") for name in files]:
+                storage[path.stem] = pygame.image.load(str(path)).convert()
+        except RuntimeError as error:
+            print(error)
+            self.status = init.EXIT
+
+        return storage
+
+    def __load_data(self):
+        """load all data from data.json file in ressources folder
+        in order to initialyse the needed class instances properly
+        abort program if files do not exists"""
+        # variable to contain all sprites
+        self.all_sprites = pygame.sprite.Group()
+
+        ressources = Path('.') / "ressources"
+        try:
+            with open(str(ressources / 'data.json'), 'r') as json_files:
+                data = json.load(json_files)
+                # Load all sprites inside a list for easy access
+                self.assets = self.__load_assets(ressources,
+                                                 data['List of sprites'])
+                # Load map of the maze store inside data.json, also contain
+                # position for Player and Guardian
+                maze = data['maze']
+
+                # Initialize player
+                index = maze.index('P')
+                self.player = Player(self.assets['player'],
+                                     (index % 15, index // 15))
+
+                # Initialize Maze
+                self.maze = Maze(maze, self.assets)
+
+                # Add sprite in sprite_group
+                for sprite in self.maze.map.values():
+                    self.all_sprites.add(sprite)
+                self.all_sprites.add(self.maze.guardian)
+                self.all_sprites.add(self.player.sprite)
+
+        except (FileNotFoundError, FileExistsError) as error:
+            print(error)
+            print("Please check ressource folder before playing Game")
+            self.status = init.EXIT
+        except KeyError as error:
+            print(f"error while attempting to read {error}" +
+                  "from data.jon file")
+            self.status = init.EXIT
+
+    def run(self):
+        """function used to mange the game display and behavior"""
+        while self.status:
+            # Keep loop running at the right speed
+            self.clock.tick(30)
+            # Process input (events)
+            for event in pygame.event.get():
+                # check for closing window
+                if event.type == pygame.QUIT:
+                    self.status = init.EXIT
+
+            # Display depending on game status
+            if self.status == init.MENU:
+                print("Menu")
+                self.status = init.PLAY
+
+            elif self.status == init.PLAY:
+                self.screen.fill(init.BLACK)
+                self.all_sprites.update()
+                self.all_sprites.draw(self.screen)
+                pygame.display.flip()
+
+            elif self.status == init.FINISH:
+                print("Finish")
+                self.status = init.EXIT
+
+        pygame.quit()
+
 
 class Sprite(pygame.sprite.Sprite):
-    """Class to manage sprite of the game with minimal information required"""
+    """Enhanced Sprite with functions to ease computation"""
     def __init__(self, img, pos):
         super().__init__()
         self.image = img
-        self.image.set_colorkey((0,0,0))
         self.rect = self.image.get_rect()
-        self.rect.x = pos[0]
-        self.rect.y = pos[1]
+        self.rect.topleft = (pos[0] * init.SPRITE_W, pos[1] * init.SPRITE_H)
+        self.image.set_colorkey(init.BLACK)
 
-    def compare_pos(self, pos):
-        """ Compare position of this sprite with position in parameter
-        return a boolean"""
-        if self.rect.x == pos[0] and self.rect.y == pos[1]:
-            return True
-        return False
+    @property
+    def pos(self):
+        """Return tuple position of sprite after conversion from pixel
+        to index referential"""
+        return (self.rect.topleft[0]//init.SPRITE_W,
+                self.rect.topleft[1]//init.SPRITE_H)
+
+    @pos.setter
+    def pos(self, pos):
+        """Set position of sprite in pixel from index tuple"""
+        self.rect.topleft = (pos[0]*init.SPRITE_W, pos[1]*init.SPRITE_H)
+
+
+class Player:
+    """Class to control the player movement, display
+    and status inside the game"""
+    def __init__(self, img, pos):
+        self.sprite = Sprite(img, pos)
+        self.objects = []
+        self.status = init.ALIVE
 
 
 class Maze:
-    """Class to manage status of the maze"""
-    def __init__(self, folder_path):
-        super().__init__()
-        #Load map, create a list of Sprite instance corresponding to the status wall or floor with right position
-        self.map = self.__init_map(folder_path)
-        self.guardian = Sprite(assets["guardian"], (14*SPRITE_WIDTH, 8*SPRITE_HEIGHT))
-        self.items = self.__init_objects()
+    """Class to control sprites of mazes, position of objects  and Guardian """
+    def __init__(self, maze, assets):
+        index = maze.index('G')
+        self.guardian = Sprite(assets['guardian'],
+                               (index % 15, index // 15))
 
-    def __init_map(self, folder_path):
-        """Load map and create Sprite instance for background
-        return a list of Sprite"""
-        result = []
-        folder_path = folder_path / 'maze.json'
-        with open(str(folder_path), 'r') as json_file:
-            data = json.load(json_file)
-            for row, line in enumerate(data["maze"]):
-                for column, val in enumerate(line):
-                    if int(val):
-                        result.append(
-                            Sprite(assets["floor"], (column*SPRITE_WIDTH, row*SPRITE_HEIGHT)))
-                    else:
-                        result.append(
-                            Sprite(assets["wall"], (column*SPRITE_WIDTH, row*SPRITE_HEIGHT)))
-        return result
-
-    def __init_objects(self):
-        """Create sprite for the 3 pickable objects with random position inside the maze, avoiding wall and guardian position
-        return list of the 3 sprites"""
-        results = []
-        sprite = ["plastic_tube", "ether", "needle"]
-        while len(results) < 3:
-            x = randint(0, 14)
-            y = randint(0, 14)
-            if (self.compare_sprite((x,y), "floor") and
-                not self.guardian.compare_pos((x,y))):
-                results.append(
-                    Sprite(assets[sprite[len(results)]], (x*SPRITE_WIDTH, y*SPRITE_HEIGHT)))
-        return results
-
-    def compare_sprite(self, pos, sprite_name):
-        """ Compare sprite in pos with the sprite given
-        return a boolean"""
-        if (self.map[pos[0]+pos[1]*15].image == assets[sprite_name]):
-            return True
-        return False
-
-class Player(Sprite):
-    """Special Sprite with additional function to allow movement depending on key press by player"""
-    def __init__(self, img, pos):
-        super().__init__(img, pos)
-        self.moving = False
-        self.target = [pos[0], pos[1]]
-        self.speed = [0,0]
-        self.alive = True
-        self.escape = False
-        self.items = []
-
-
-    def update(self):
-        if self.moving:
-            self.rect.x += self.speed[0]
-            self.rect.y += self.speed[1]
-            if self.target == [self.rect.x, self.rect.y]:
-                self.moving = False
-                self.speed[0] = 0
-                self.speed[1] = 0
-        else:
-            self.check_guardian()
-            if len(self.items) < 3:
-                self.check_objects()
+        self.map = {}
+        for index, val in enumerate(maze):
+            pos = (index % 15, index // 15)
+            if not val:
+                self.map[pos] = Sprite(assets["wall"], pos)
             else:
-                self.items.append(
-                    Sprite(assets["syringe"], (5*SPRITE_WIDTH, 15*SPRITE_HEIGHT)))
-                #to be modify
-                all_sprites.add(self.items[3])
-            self.check_mov()
-
-    def check_guardian(self):
-        """ Check if guardian if guardian is in cell and depending on number of objects in possession of player
-        kill guardian or player"""
-        if (maze.guardian.rect.x == self.rect.x and 
-        maze.guardian.rect.y == self.rect.y):
-            if len(self.items) > 2:
-                self.escape = True
-            else:
-                self.alive = False
-
-    def check_objects(self):
-        for i,item in enumerate(maze.items[:]):
-            if (item.rect.x == self.rect.x and
-            item.rect.y == self.rect.y):
-                self.items.append(maze.items.pop(i))
-                #Modify position of items to place it our of maze
-                index = len(self.items) - 1
-                self.items[index].rect.x = index * SPRITE_WIDTH
-                self.items[index].rect.y = 15 * SPRITE_HEIGHT
-
-    def check_mov(self):
-        """fonction that check the validity of move of the player and change target, speed and movement status if movement allow"""
-        keystate = pygame.key.get_pressed()
-        if (keystate[pygame.K_LEFT] and
-        self.rect.left > 0 and
-        maze.compare_sprite(self.pos_left, "floor")):
-            self.target[0] -= SPRITE_WIDTH
-            self.speed[0] = -SPRITE_WIDTH//4
-            self.moving = True
-
-        if (keystate[pygame.K_RIGHT] and
-        self.rect.right < WIDTH - WIDTH_OPTION and 
-        maze.compare_sprite(self.pos_right, "floor")):
-            self.target[0] += SPRITE_WIDTH
-            self.speed[0] = +SPRITE_WIDTH//4
-            self.moving = True
-
-        if (keystate[pygame.K_UP] and
-        self.rect.top > 0 and
-        maze.compare_sprite(self.pos_up, "floor")):
-            self.target[1] -= SPRITE_HEIGHT
-            self.speed[1] = -SPRITE_HEIGHT//4
-            self.moving = True
-
-        if (keystate[pygame.K_DOWN] and
-        self.rect.bottom < HEIGHT - HEIGHT_OPTION and
-        maze.compare_sprite(self.pos_down, "floor")):
-            self.target[1] += SPRITE_HEIGHT
-            self.speed[1] = +SPRITE_HEIGHT//4
-            self.moving = True
-
-    @property
-    def pos_left(self):
-        """Return tuple of index from sprite at the left of player """
-        return ((self.rect.x-SPRITE_WIDTH)//SPRITE_WIDTH, self.rect.y//SPRITE_HEIGHT)
-
-    @property
-    def pos_right(self):
-        """Return tuple of index from sprite at the left of player """
-        return ((self.rect.x+SPRITE_WIDTH)//SPRITE_WIDTH, self.rect.y//SPRITE_HEIGHT)
-
-    @property
-    def pos_up(self):
-        """Return tuple of index from sprite at the left of player """
-        return (self.rect.x//SPRITE_WIDTH, (self.rect.y-SPRITE_HEIGHT)//SPRITE_HEIGHT)
-
-    @property
-    def pos_down(self):
-        """Return tuple of index from sprite at the left of player """
-        return (self.rect.x//SPRITE_WIDTH, (self.rect.y+SPRITE_HEIGHT)//SPRITE_HEIGHT)
+                self.map[pos] = Sprite(assets["floor"], pos)
 
 
-# initialize pygame and create Game Window
-pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("My Game")
-clock = pygame.time.Clock()
-
-#Load assets
-ressources = Path(".").joinpath("ressources")
-assets = load_assets(ressources)
-
-#Instance for main classes
-maze = Maze(ressources)
-player = Player(assets["player"], (2*SPRITE_WIDTH, 1*SPRITE_HEIGHT))
-
-#Load Sprites in group
-all_sprites = pygame.sprite.Group()
-for sprite in maze.map:
-    all_sprites.add(sprite)
-all_sprites.add(maze.guardian)
-
-for sprite in maze.items:
-    all_sprites.add(sprite)
-all_sprites.add(player)
-
-# Game loop
-running = True
-while running:
-    # keep loop running at the right speed
-    clock.tick(FPS)
-    # Process input (events)
-    for event in pygame.event.get():
-        # check for closing window
-        if event.type == pygame.QUIT:
-            running = False
-
-    if player.escape:
-        running = False
-        print("You Win")
-
-    if not player.alive:
-        running = False
-        print("You Lose")
-
-    # Update all sprites status
-    all_sprites.update()
-
-    # Draw / render
-    screen.fill((0,0,0))
-
-    all_sprites.draw(screen)
-
-    #Display changes on screen
-    pygame.display.flip()
-
-pygame.quit()
+if __name__ == "__main__":
+    game = Game()
+    game.run()
